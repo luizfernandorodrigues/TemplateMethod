1af1451d28cae18de2a501afd1497badcdd06f7e
Luiz Fernando <luizfernando_rodrigues12@hotmail.com>
2020-04-09 20:23:10 -0300

    Add project files.

---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractClass abstractClass = new ConcreteClassA();
+            abstractClass.TemplateMethod();
+
+            AbstractClass abstractClass1 = new ConcreteClassB();
+            abstractClass1.TemplateMethod();
+
+
+            Console.ReadKey();
+        }
+    }
+}
